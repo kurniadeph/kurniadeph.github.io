17edcbc0bfd4bcda3359b44a7bd972c28783b47c
Kurnia DC. Putra <39754322+kurniadeph@users.noreply.github.com>
2023-09-13 16:10:44 +0700

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -32,6 +32,76 @@ let typingEffect = new Typed(".typeText",{
     backSpeed : 80,
     backDelay : 2000
 })
+
+/* -----# SCROLLING ANIMATION #----- */
+const sr = ScrollReveal({
+      origin = 'top',
+      distance = '80px',
+      duration = 2000,
+      reset = true
+})
 /* ----- HOME ----- */
 sr.reveal('.style-text-box',{})
-sr.reveal('.style--text-name',{delay: 100})
+sr.reveal('.style-text-name',{delay: 100})
+sr.reveal('.style-text-caption',{delay: 200})
+sr.reveal('.style-text-btn',{delay: 200})
+sr.reveal('.social-icon',{delay: 200})
+sr.reveal('.style-image',{delay: 300})
+
+/* ----- Project Box ----- */
+sr.reveal('.col-project',{interval: 200})
+
+/* ----- Heading ----- */
+sr.reveal('.top-header',{})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srleft.reveal('.profil-info',{delay: 100})
+srleft.reveal('.kontak-info',{delay: 100})
+
+/* -- Skill & Kontak Info -- */
+
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srleft.reveal('.skill-box',{delay: 100})
+srleft.reveal('.form-card',{delay: 100})
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+
+    }  else {
+
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
